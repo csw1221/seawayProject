8b7902ace855677c8ada4d00e98dd02f5d3fc749
csw1221 <csw1221@qq.com>
2015-02-09 10:50:18 +0800

    实例项目+修复CSS合并问题
    1、增加实力
    2、修复CSS合并 文件后边多分号

--- src/js/base.js
+++ src/js/base.js
@@ -1,11 +1,65 @@
 /**
  *
  * @authors Your Name (you@example.org)
- * @date    2015-02-03 11:58:00
+ * @date    2014-09-15 09:37:13
  * @version $Id$
  */
 
-$(function(){
-	alert("cc");
-	var cc="dsas";
-});
+xtwgw = {
+	scrollToPage: function(value) {
+		var o = {
+			oContent: "#bannerTemplate", //插入的对象
+			nCount: 4 //jsonpCallback
+		};
+		o = $.extend(o, value);
+		o.nCount=$("#bannerTemplate").find('li').length;
+		i = 0;
+		var wdc=$("#bannerTemplate").width();
+		$("#bannerTemplate").height(wdc/1.6);
+		$("#bannerTemplate img").css("height",wdc/1.6);
+		$("#bannerTemplate ul").css("height", wdc/1.6);
+		$("#bannerTemplate").find("li").eq(0).siblings().addClass("next");
+		
+		var hover = setInterval(function() {
+			if (i > o.nCount - 1) {
+				i = 0;
+			}
+			p=i-1;
+			if(p>o.nCount - 1){
+				p=0;
+			}
+			n=i+1;
+			if(n>o.nCount - 1){
+             n=0;
+			}
+			$("#iconlist").find("i").eq(n).addClass('cur').siblings().removeClass('cur');
+            $("#bannerTemplate").find('li').eq(p).removeClass('pre').addClass('next');
+			$("#bannerTemplate").find('li').eq(i).addClass('pre');
+			$("#bannerTemplate").find('li').eq(n).removeClass('next');
+			i++;
+		}, 2000);
+        $(o.oContent).bind("touchstart",function(){
+           clearInterval(hover);
+         });
+        $(o.oContent).bind("touchend",function(){
+           hover = setInterval(function() {
+			if (i > o.nCount - 1) {
+				i = 0;
+			}
+			p=i-1;
+			if(p>o.nCount - 1){
+				p=0;
+			}
+			n=i+1;
+			if(n>o.nCount - 1){
+             n=0;
+			}
+			$("#iconlist").find("i").eq(i).addClass('cur').siblings().removeClass('cur');
+            $("#bannerTemplate").find('li').eq(p).removeClass('pre').addClass('next');
+			$("#bannerTemplate").find('li').eq(i).addClass('pre');
+			$("#bannerTemplate").find('li').eq(n).removeClass('next');
+			i++;
+		}, 2000);
+         });
+	}
+};
